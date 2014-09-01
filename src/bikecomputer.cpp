ff25b9aa4810d33f49f17552d521285a8e1a290f
Paul Osborne <osbpau@gmail.com>
2014-08-31 21:00:42 -0500

    reed switch: modify reed switch to change led based on interrupts

--- src/bikecomputer.cpp
+++ src/bikecomputer.cpp
@@ -1,24 +1,34 @@
 #include "mbed.h"
 
+static const PinName PIN_REED_SWITCH = PTD5;
+
 DigitalOut g(LED_GREEN);
 DigitalOut r(LED_RED);
 DigitalOut b(LED_BLUE);
-DigitalIn gpio_d5(PTD5);
+InterruptIn reed_switch_interrupt(PIN_REED_SWITCH);
+
 
 static uint8_t const LED_OFF = 1;
 static uint8_t const LED_ON = 0;
 
+static void reed_switch_rising()
+{
+    g.write(LED_ON);
+}
+
+static void reed_switch_falling()
+{
+    g.write(LED_OFF);
+}
+
 int main()
 {
+    reed_switch_interrupt.rise(&reed_switch_rising);
+    reed_switch_interrupt.fall(&reed_switch_falling);
     g.write(LED_OFF);
     r.write(LED_OFF);
     b.write(LED_OFF);
     while (true) {
-        if (gpio_d5) {
-            g.write(LED_ON);
-        } else {
-            g.write(LED_OFF);
-        }
         wait_ms(50);
     }
 }
