0c76065af6ab583f8890aeca7974a7f613997727
Paul Osborne <osbpau@gmail.com>
2014-09-01 23:34:19 -0500

    core: write down pin mappings so that are not forgotten

--- src/bikecomputer.cpp
+++ src/bikecomputer.cpp
@@ -1,24 +1,36 @@
 #include "mbed.h"
 #include "debug.h"
 
-static const PinName PIN_REED_SWITCH = PTD5;
-static const PinName PIN_SPI_SCK = PTD1;
-static const PinName PIN_SPI_MOSI = PTD2;
-static const PinName PIN_SPI_MISO = PTD3;
+/* Reed Switch Input (Spinning of the Wheel) */
+static const PinName PIN_REED_SWITCH_GPIO = PTD5;
+InterruptIn reed_switch_interrupt(PIN_REED_SWITCH_GPIO);
+
+/* NRF8001 Bluetooth LE Module (Adafruit Bluefruit LE) */
+static const PinName PIN_NRF8001_SPI_SCK = PTD1; // yellow
+static const PinName PIN_NRF8001_SPI_MOSI = PTD2; // brown
+static const PinName PIN_NRF8001_SPI_MISO = PTD3; // gray
+static const PinName PIN_NRF8001_GPIO_REQ = PTC9; // blue
+static const PinName PIN_NRF8001_GPIO_RDY = PTC8; // white
+static const PinName PIN_NRF8001_GPIO_ACT = PTA5; // purple
+static const PinName PIN_NRF8001_GPIO_RST = PTA4; // orange
+static const DigitalInOut nrf8001_gpio_req(PIN_NRF8001_GPIO_REQ);
+static const DigitalInOut nrf8001_gpio_rdy(PIN_NRF8001_GPIO_RDY);
+static const DigitalInOut nrf8001_gpio_act(PIN_NRF8001_GPIO_ACT);
+static const DigitalInOut nrf8001_gpio_rst(PIN_NRF8001_GPIO_RST);
 
 /* Debug UART */
-static const PinName PIN_SERIAL_RX = PTA1;
-static const PinName PIN_SERIAL_TX = PTA2;
+static const PinName PIN_SERIAL_RX = PTA1; // green (to TTL converter)
+static const PinName PIN_SERIAL_TX = PTA2; // white (to TTL converter)
 static Serial debugSerialPort(PIN_SERIAL_TX, PIN_SERIAL_RX, "debug");
 static DebugSerialPort dbg(debugSerialPort);
 
-DigitalOut g(LED_GREEN);
-DigitalOut r(LED_RED);
-DigitalOut b(LED_BLUE);
-InterruptIn reed_switch_interrupt(PIN_REED_SWITCH);
-
+/* FRDM board LED */
+DigitalOut frdm_led_green_channel(LED_GREEN);
+DigitalOut frdm_led_red_channel(LED_RED);
+DigitalOut frdm_led_blue_channel(LED_BLUE);
 static uint8_t const LED_OFF = 1;
 static uint8_t const LED_ON = 0;
+
 
 static void debug_serial_init()
 {
@@ -27,13 +39,13 @@ static void debug_serial_init()
 
 static void reed_switch_rising()
 {
-    g.write(LED_ON);
+    frdm_led_green_channel.write(LED_ON);
     dbg.writeln("Reed Switch Rising Edge");
 }
 
 static void reed_switch_falling()
 {
-    g.write(LED_OFF);
+    frdm_led_green_channel.write(LED_OFF);
     dbg.writeln("Reed Switch Falling Edge");
 }
 
@@ -42,9 +54,9 @@ int main()
     debug_serial_init();
     reed_switch_interrupt.rise(&reed_switch_rising);
     reed_switch_interrupt.fall(&reed_switch_falling);
-    g.write(LED_OFF);
-    r.write(LED_OFF);
-    b.write(LED_OFF);
+    frdm_led_green_channel.write(LED_OFF);
+    frdm_led_red_channel.write(LED_OFF);
+    frdm_led_blue_channel.write(LED_OFF);
     while (true) {
         wait_ms(50);
     }
